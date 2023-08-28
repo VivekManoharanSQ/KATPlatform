3470eda8f688a48de02c7c56cf21d6b7ef87d3cd
Vivek <vivek.manoharan@skillquotient.net>
2023-08-28 15:20:11 +0800

    new bdd suite

--- Test Suites/BDDSuite.ts
+++ Test Suites/BDDSuite.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>f1f117c0-231a-4192-82e0-43de1ba7038a</testSuiteGuid>
+   <testCaseLink>
+      <guid>0d6aebc1-d03e-45eb-8c51-22de90f46e2f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/BDDTest</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
